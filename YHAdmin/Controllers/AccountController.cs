6f4ba4205068d56765a8fe538cd5152daa9f061f
Anders Eklöf <anderse77@gmail.com>
2015-11-30 14:08:56 +0100

    Har satt upp SimpleMemberShipProvider. Implementationen fungerar inte utan rätt rader. Web.Config. Så snart jag får access till databasen kommer jag att lägga in användare och roller.

---
+++ YHAdmin/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using WebMatrix.WebData;
+
+namespace YHAdmin.Controllers
+{
+    class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Login loginCredentials)
+        {
+            if (ModelState.IsValid)
+            {
+                if (WebSecurity.Login(loginCredentials.UserName, loginCredentials.Password))
+                {
+                    //Exakt vad som händer när en användare lyckas logga in kan ändras i ett senare skede.
+                    return RedirectToAction("Index", "Home");
+                }
+                    ModelState.AddModelError("", "Felaktigt användarnamn eller lösenord.");
+                    return View(loginCredentials);
+            }
+            return View(loginCredentials);
+        }
+    }
+}
